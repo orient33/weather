f3560af5442e026566298b36988ffd58af97a2f2
dfdun <dfdun@ingenic.cn>
2013-12-16 16:13:31 +0800

    refresh weather info when bind firstly; fix bug2013121411678
    Change-Id: I6a7909c33ee46f4f6a2fb83ac0b5221a268fc829

--- src/cn/ingenic/remotemanager/Commands.java
+++ src/cn/ingenic/remotemanager/Commands.java
@@ -1,13 +1,16 @@
 package cn.ingenic.remotemanager;
 
 public class Commands {
+	public static final String TAG="sw2df";
 	//sync
 	public final static String ACTION_SYNC = "cn.ingenic.action.sync";
 	
 	//weather
 	public final static String ACTION_WEATHER = "cn.ingenic.action.weather";
 	public final static int CMD_INTERNET_REQUEST = 1;
-	
+	/* first bind between watch and phone*/
+	public final static String ACTION_BIND = "cn.ingenic.action.weather.first.bind";
+
 	//device manager
 	public final static String ACTION_DEVICE_MANAGER = "cn.ingenic.action.devicemanager";
 	public final static int CMD_LOCK_SCREEN = 2;
--- src/cn/ingenic/weather/CommandReceiver.java
+++ src/cn/ingenic/weather/CommandReceiver.java
@@ -1,16 +1,30 @@
 package cn.ingenic.weather;
 
+import cn.ingenic.remotemanager.Commands;
+import cn.ingenic.weather.engine.City;
 import weathersource.webxml.com.cn.InternetAccess;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 
 public class CommandReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		if(Commands.ACTION_BIND.equals(intent.getAction())){
+			EngineManager em=EngineManager.getInstance(context);
+			City city = em.getDefaultMarkCity();
+			if(city!=null)
+				em.getWeatherByIndex(city.index);
+			else
+				em.getWeatherByIndex("792");// 792 ,also beijing, is default city
+		    Log.i(Commands.TAG,"[first bind]receive: "+Commands.ACTION_BIND+";"+city);
+			return;
+		}
+		
 		Bundle bundle = intent.getExtras();
 		InternetAccess.onInternetResponse(bundle.getInt("cmd"), bundle.getString("data"));
 		Long.valueOf("1111");
